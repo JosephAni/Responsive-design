9c231461ce09a8f49da44fdb1433d63532f0fe1e
Joseph Ani <joeoani@gmail.com>
2021-04-27 11:12:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ emailCollectorForm.addEventListener("submit", event => {
     let updatedHtmlContent = `
         <h2>Congratulations, ${userFirstName}!</h2>
 
-        <p>You're on your way to becoming a BBQ Master!</p>
+        <p>You're on your way to becoming a shrimp Master!</p>
         
         <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>
     `
